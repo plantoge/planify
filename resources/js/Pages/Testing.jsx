f7a4255c4af11f6f90b03cb2ecc34ae70d812c3c
Fauzi <arsipfauzi@gmail.com>
2024-12-11 09:45:42 +0700

    new app layout

---
+++ resources/js/Pages/Testing.jsx
@@ -0,0 +1,12 @@
+import AppLayout from '@/Layouts/AppLayout'
+import React from 'react'
+
+const Testing = () => {
+  return (
+    <AppLayout>
+        <div>Testing</div>
+    </AppLayout>
+  )
+}
+
+export default Testing
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ Route::get('/', function () {
     ]);
 });
 
+route::get('testing', function () {
+    return Inertia::render('Testing');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
