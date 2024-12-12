dfab23c06b9d1c989216a3b450779513a5572358
Fauzi <arsipfauzi@gmail.com>
2024-12-12 14:16:23 +0700

    refactoring page verify email

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,6 +1,7 @@
-import PrimaryButton from '@/Components/PrimaryButton';
+import { Button } from '@/Components/ui/button';
+import { Card, CardContent, CardHeader } from '@/Components/ui/card';
 import GuestLayout from '@/Layouts/GuestLayout';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { Link, useForm } from '@inertiajs/react';
 
 export default function VerifyEmail({ status }) {
     const { post, processing } = useForm({});
@@ -12,39 +13,46 @@ export default function VerifyEmail({ status }) {
     };
 
     return (
-        <GuestLayout>
-            <Head title="Email Verification" />
-
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify
-                your email address by clicking on the link we just emailed to
-                you? If you didn't receive the email, we will gladly send you
-                another.
+        <GuestLayout title="Verify Email">
+            <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <Card>
+                        <CardHeader>
+                            <Link href="/" className="text-4xl font-black leading-relaxed tracking-tighter">
+                                Plannify<span className="text-red-500">.</span>
+                            </Link>
+                            <h2 className="text-lg font-medium leading-9 tracking-tight text-left text-muted-foreground">
+                                Thanks for signing up! Before getting started, could you verify your email address by clicking on
+                                the link we just emailed to you? If you didn't receive the email, we will gladly send you another.
+                            </h2>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="mt-6 sm:mx-auto sm:w-full sm:max-w-sm">
+                                {status === 'verification-link-sent' && (
+                                    <div className="mb-4 text-sm font-medium text-green-600">
+                                        A new verification link has been sent to the email address you provided during registration.
+                                    </div>
+                                )}
+                                <form className="space-y-6" onSubmit={submit}>
+                                    {/* form */}
+                                    <div className='flex items-center justify-between mt-4'>
+                                        <Button disabled={processing}>
+                                            Resend  Verification
+                                        </Button>
+                                        <Link 
+                                            href={route('logout')} 
+                                            method="post" 
+                                            as="button" 
+                                            className='text-sm underline rounded-md text-muted-foreground hover: focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2'>
+                                            Logout
+                                        </Link>
+                                    </div>
+                                </form>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
-
-            {status === 'verification-link-sent' && (
-                <div className="mb-4 text-sm font-medium text-green-600">
-                    A new verification link has been sent to the email address
-                    you provided during registration.
-                </div>
-            )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>
-                        Resend Verification Email
-                    </PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    >
-                        Log Out
-                    </Link>
-                </div>
-            </form>
         </GuestLayout>
     );
 }
