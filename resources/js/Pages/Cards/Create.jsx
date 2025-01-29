ed4eb4000734ad6dff04c286be70aec21d81ab43
Fauzi <50617753+plantoge@users.noreply.github.com>
2025-01-29 21:15:15 +0700

    proggress tambah card 2
    menit 03:00

--- resources/js/Pages/Cards/Create.jsx
+++ resources/js/Pages/Cards/Create.jsx
@@ -1,4 +1,7 @@
 import HeaderForm from '@/Components/HeaderForm'
+import InputLabel from '@/Components/InputLabel'
+import TextInput from '@/Components/TextInput'
+import { Card, CardContent } from '@/Components/ui/card'
 import AppLayout from '@/Layouts/AppLayout'
 import React from 'react'
 
@@ -7,7 +10,40 @@ export default function Create({page_settings, Statuses, Priorities, workspace})
     <div className='space-y-10 divide-y divide-dashed divide-gray-900/10'>
         <div className='grid grid-cols-1 gap-x-4 gap-y-8 md:grid-cols-3'>
             <HeaderForm title={page_settings.title} subtitle={page_settings.subtitle} />
-            {/* <EditCard Statuses={Statuses} Priorities={Priorities} workspace={workspace} /> */}
+            <Card className='md:col-span-2' >
+                <CardContent>
+                    <form>
+                        <div className="py-6">
+                            <div className='grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
+                                <div className='col-span-full'>
+                                    <InputLabel htmlfor='title' value='Title' />
+                                    <TextInput 
+                                        type="text"
+                                        name='title'
+                                        id='title'
+                                    />
+                                </div>
+                                <div className='col-span-full'>
+                                    <InputLabel htmlfor='description' value='Description' />
+                                    <TextInput 
+                                        type="text"
+                                        name='description'
+                                        id='description'
+                                    />
+                                </div>
+                                <div className='col-span-full'>
+                                    <InputLabel htmlfor='deadline' value='Deadline' />
+                                    <TextInput 
+                                        type="date"
+                                        name='deadline'
+                                        id='deadline'
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+            </Card>
         </div>
     </div>
   )
